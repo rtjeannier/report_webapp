cd0bc95535a83630734bf9fae52b7daaa4ea6672
roland jeannier <rtjeannier@gmail.com>
2019-06-15 13:17:44 -0400

    unding hello world app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DataViz msg="THIS WILL THE TEXT ON MY PAGE"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataViz from './components/DataViz.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DataViz
   }
 }
 </script>
---
+++ src/components/DataViz.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-div">
+        text here
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DataViz',
+  props: {
+    msg: String
+  }     
+}
+</script>
+
